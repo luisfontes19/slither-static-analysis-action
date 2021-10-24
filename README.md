eb6ba24f5d086f47afbbc05bcdeba7a7df0ed86b
Scott Nixon <citadelgrad@gmail.com>
2021-10-24 14:51:39 -0700

    fix: override the projectPath when provided.
    Upgrade to latest version of slither.

--- README.md
+++ README.md
@@ -7,56 +7,55 @@ This action is a wrapper for slither, a static vulnerability tool for Solidity s
 ### `run-npm-install`
 
 By default the action will run npm install on the source folder. Set this variable as false to disable it
-***Default:*** true
-***Required:*** no
+**_Default:_** true
+**_Required:_** no
 
 ### `slither-version`
 
 Specify slither version to use. It will download it from official github project.
-***Default:*** "0.6.14"
-***Required:*** no
-
+**_Default:_** "0.8.1"
+**_Required:_** no
 
 ### `high-threshold`
 
 Action will fail if the number of High findings is equal or bigger then this value (0 to disable)
-***Default:*** 1
-***Required:*** no
+**_Default:_** 1
+**_Required:_** no
 
 ### `medium-threshold`
 
 Action will fail if the number of Medium findings is equal or bigger then this value (0 to disable)
-***Default:*** 1
-***Required:*** no
+**_Default:_** 1
+**_Required:_** no
 
 ### `low-threshold`
 
 Action will fail if the number of Low findings is equal or bigger then this value (0 to disable)
-***Default:*** 1
-***Required:*** no
+**_Default:_** 1
+**_Required:_** no
 
 ### `optimization-threshold`
 
 Action will fail if the number of Optimization findings is equal or bigger then this value (0 to disable)
-***Default:*** 1
-***Required:*** no
+**_Default:_** 1
+**_Required:_** no
 
 ### `informative-threshold`
 
 Action will fail if the number of Informative findings is equal or bigger then this value (0 to disable)
-***Default:*** 10
-***Required:*** no
+**_Default:_** 10
+**_Required:_** no
 
 ### `projectPath`
 
 The path to the smart contract's project
-***Default:*** "."
-***Required:*** no
+**_Default:_** "."
+**_Required:_** no
 
 ### `slither-params`
 
-Extra slither params to be appended. By default the action runnins with: ```slither --json - .```
-***Required:*** no
+Extra slither params to be appended. By default the action runs with: `slither --json - ${projectPath}`
+**_Required:_** no
 
 ## Example usage
 
@@ -72,14 +71,14 @@ on: [push]
 jobs:
   main_job:
     runs-on: ubuntu-latest
-    name: Solidity Security 
+    name: Solidity Security
     steps:
       - name: Checkout
         uses: actions/checkout@v2
       - name: Slither Static Analysis
         uses: luisfontes19/slither-static-analysis-action@v0.3.2
         with:
-          slither-version: '0.6.13'
+          slither-version: "0.8.1"
           run-npm-install: true
           high-threshold: 1
           medium-threshold: 1
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
 
 const slitherVersion = core.getInput("slither-version") || "0.6.14";
 const runNpmInstall = core.getInput("run-npm-install") === "true";
 const failOnHighResults = parseInt(core.getInput("high-threshold")) || 1;
 const failOnMediumResults = parseInt(core.getInput("medium-threshold")) || 1;
 const failOnLowResults = parseInt(core.getInput("low-threshold")) || 1;
-const failOnInformativeResults = parseInt(core.getInput("informative-threshold")) || 10;
-const failOnOptimizationResults = parseInt(core.getInput("optimization-threshold")) || 1;
+const failOnInformativeResults =
+  parseInt(core.getInput("informative-threshold")) || 10;
+const failOnOptimizationResults =
+  parseInt(core.getInput("optimization-threshold")) || 1;
 const slitherParams = core.getInput("slither-params") || "";
 const projectPath = core.getInput("projectPath") || ".";
 
 type Severity = "High" | "Medium" | "Low" | "Informational" | "Optimization";
 
 const run = async () => {
-
-
-  const counts = { "High": 0, "Medium": 0, "Low": 0, "Informational": 0, "Optimization": 0 };
+  const counts = {
+    High: 0,
+    Medium: 0,
+    Low: 0,
+    Informational: 0,
+    Optimization: 0,
+  };
 
   printDebugInfo();
 
   try {
     await prepare();
-  }
-  catch (ex) {
+  } catch (ex) {
     core.setFailed("Something went wrong. Check above for more information");
     return;
   }
@@ -44,9 +49,9 @@ const run = async () => {
       return;
     }
 
-    console.log("")
+    console.log("");
     console.log("----------------------------------------");
-    console.log("                Findings")
+    console.log("                Findings");
     console.log("----------------------------------------");
     (data.results.detectors || []).forEach((d: any) => {
       const severity = d.impact as Severity;
@@ -59,37 +64,38 @@ const run = async () => {
       console.log("");
     });
 
-
     showStats(counts);
-  }
-  catch (ex) {
+  } catch (ex) {
     core.setFailed("Error parsing results");
     console.debug(ex);
     return;
   }
-}
+};
 
 const prepare = async () => {
   core.info("Install dependencies");
-  await exec.exec("sudo apt-get install -y git python3 python3-setuptools wget software-properties-common");
+  await exec.exec(
+    "sudo apt-get install -y git python3 python3-setuptools wget software-properties-common"
+  );
 
   core.info("Downloading slither");
-  await exec.exec(`wget https://github.com/crytic/slither/archive/${slitherVersion}.zip -O /tmp/slither.zip`);
-
+  await exec.exec(
+    `wget https://github.com/crytic/slither/archive/${slitherVersion}.zip -O /tmp/slither.zip`
+  );
 
   core.info("Unzipping slither");
   await exec.exec(" unzip /tmp/slither.zip -d .");
 
-
   core.info("Installing slither");
-  await exec.exec("sudo python3 setup.py install", undefined, { cwd: `slither-${slitherVersion}` });
-
+  await exec.exec("sudo python3 setup.py install", undefined, {
+    cwd: `slither-${slitherVersion}`,
+  });
 
   if (runNpmInstall) {
     core.info("Installing dependencies");
     await exec.exec("npm install", undefined, { cwd: projectPath });
   }
-}
+};
 
 const printDebugInfo = () => {
   core.debug("Configs:");
@@ -101,37 +107,61 @@ const printDebugInfo = () => {
   core.debug("failOnOptimizationResults: " + failOnOptimizationResults);
   core.debug("projectPath: " + projectPath);
   core.debug("runNpmInstall: " + runNpmInstall);
-}
+};
 
 const runSlither = async (): Promise<string> => {
-  //slither return code is the number of findings... 
+  //slither return code is the number of findings...
   return new Promise((resolve, reject) => {
     let output = "";
     const options: exec.ExecOptions = {};
     options.listeners = {
-      stdout: (data: Buffer) => output += data.toString()
+      stdout: (data: Buffer) => (output += data.toString()),
     };
     options.cwd = projectPath;
 
-    exec.exec("slither --json - . " + slitherParams, undefined, options).then(() => resolve(output)).catch(() => resolve(output));
-  })
-}
+    exec
+      .exec(
+        `slither --json - ${projectPath} ` + slitherParams,
+        undefined,
+        options
+      )
+      .then(() => resolve(output))
+      .catch(() => resolve(output));
+  });
+};
 
 //TODO: replace any
 const showStats = (counts: any) => {
-  core.warning(`Stats: High: ${counts["High"]}, Medium: ${counts["Medium"]}, Low: ${counts["Low"]}, Informative: ${counts["Informational"]}, Optimizations: ${counts["Optimization"]}`);
+  core.warning(
+    `Stats: High: ${counts["High"]}, Medium: ${counts["Medium"]}, Low: ${counts["Low"]}, Informative: ${counts["Informational"]}, Optimizations: ${counts["Optimization"]}`
+  );
 
   if (failOnHighResults !== 0 && failOnHighResults <= counts["High"])
-    core.setFailed(`Number of High results is equal or bigger then the defined threshold of ${failOnHighResults}`);
+    core.setFailed(
+      `Number of High results is equal or bigger then the defined threshold of ${failOnHighResults}`
+    );
   else if (failOnMediumResults !== 0 && failOnMediumResults <= counts["Medium"])
-    core.setFailed(`Number of Medium results is equal or bigger then the defined threshold of ${failOnMediumResults}`);
+    core.setFailed(
+      `Number of Medium results is equal or bigger then the defined threshold of ${failOnMediumResults}`
+    );
   else if (failOnLowResults !== 0 && failOnLowResults <= counts["Low"])
-    core.setFailed(`Number of Low results is equal or bigger then the defined threshold of ${failOnLowResults}`);
-  else if (failOnInformativeResults !== 0 && failOnInformativeResults <= counts["Informational"])
-    core.setFailed(`Number of Informative results is equal or bigger then the defined threshold of ${failOnInformativeResults}`);
-  else if (failOnOptimizationResults !== 0 && failOnOptimizationResults <= counts["Optimization"])
-    core.setFailed(`Number of Optimization results is equal or bigger then the defined threshold of ${failOnOptimizationResults}`);
-}
-
+    core.setFailed(
+      `Number of Low results is equal or bigger then the defined threshold of ${failOnLowResults}`
+    );
+  else if (
+    failOnInformativeResults !== 0 &&
+    failOnInformativeResults <= counts["Informational"]
+  )
+    core.setFailed(
+      `Number of Informative results is equal or bigger then the defined threshold of ${failOnInformativeResults}`
+    );
+  else if (
+    failOnOptimizationResults !== 0 &&
+    failOnOptimizationResults <= counts["Optimization"]
+  )
+    core.setFailed(
+      `Number of Optimization results is equal or bigger then the defined threshold of ${failOnOptimizationResults}`
+    );
+};
 
 run();
